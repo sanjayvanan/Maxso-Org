4ab460e00a655a8355b45e7b3a7c60c8e3678747
Sanjayvanan <sanjayvanan03@gmail.com>
2026-03-03 21:27:51 +0530

    feat : added new orgin

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,8 @@ app.use(cors({
     'http://localhost:5173',
     'http://localhost:5174',
     'http://localhost:5175',
-    'https://maxso-org.onrender.com'
+    'https://maxso-org.onrender.com',
+    'https://maxso-org.vercel.app'
   ],
   credentials: true
 }));
