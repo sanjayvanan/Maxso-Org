925af101b3a0f5bc0793cb7c92db7cf6df46a1b0
Sanjayvanan <sanjayvanan03@gmail.com>
2026-03-03 21:22:48 +0530

    feat: Added more orgins

--- backend/server.js
+++ backend/server.js
@@ -11,9 +11,15 @@ const app = express()
 
 // middleware
 app.use(cors({
-  origin: 'http://localhost:5174',
+  origin: [
+    'http://localhost:5173',
+    'http://localhost:5174',
+    'http://localhost:5175',
+    'https://maxso-org.onrender.com'
+  ],
   credentials: true
 }));
+
 app.use(express.json())
 app.use(cookieParser());
 
